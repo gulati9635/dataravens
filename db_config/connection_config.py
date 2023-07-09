5c5bb4d66a47b9de96fc64fe0e7e4d0c32bd3db5
Mohit <gulati9635@gmail.com>
2023-07-09 18:43:49 +0530

    files updated

--- db_config/connection_config.py
+++ db_config/connection_config.py
@@ -1,4 +1,10 @@
+# server = "dentallyprojectserver.database.windows.net"
+# database = 'SQLDB'
+# port = '1433'
+# driver = "odbc Driver 18 for SQL Server"
+
+
 server = "dentallyprojectserver.database.windows.net"
-database = 'SQLDB'
-port = '1433'
+database = 'SQL_DB'
+port = '3306'
 driver = "odbc Driver 18 for SQL Server"
