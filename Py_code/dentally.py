a264d8425338322a1bf3337c33d0ded8ef7db84d
Mohit Gulati <gulati9635@gmail.com>
2023-07-15 00:35:01 +0100

    json write and dataframe view

--- Py_code/dentally.py
+++ Py_code/dentally.py
@@ -1,10 +1,16 @@
-
 import utils.dbutils as db
 import db_config.connection_config as conn
 import db_config.credentials as cred
 import webbrowser
 import utils.apiutils as apu
 import api_config.apiconfig as ap
+import pandas as pd
+# import pymysql
+from datetime import datetime
+import sqlalchemy
+
+
+
 # db.connect_azure_sql(conn.driver
 #                      ,conn.server
 #                      ,conn.port
@@ -12,13 +18,13 @@ import api_config.apiconfig as ap
 #                      ,cred.username
 #                      ,cred.password)
 
-db.connect_to_mysql(conn.host,cred.user,cred.password,conn.database)
+# db.connect_to_mysql(conn.host, cred.user, cred.password, conn.database)
 
 authorization_url = apu.authorization_url_fn(ap.authorization_endpoint,
-                      ap.client_id,
-                      ap.redirect_uri,
-                      ap.scope,
-                      ap.response_type)
+                                             ap.client_id,
+                                             ap.redirect_uri,
+                                             ap.scope,
+                                             ap.response_type)
 
 webbrowser.open(authorization_url)
 
@@ -33,14 +39,19 @@ token_data = {
     'client_id': ap.client_id,
     'client_secret': ap.client_secret
 }
-token_generated = apu.token_data(ap.token_endpoint,token_data)
+token_generated = apu.token_data(ap.token_endpoint, token_data)
 
-data = apu.get_api(token_generated,ap.patient_read_endpoint)
+data = apu.get_api(token_generated, ap.patient_read_endpoint)
 
-import json
+table_name = key = list(data.keys())[0]
+print(table_name)
 
-json_object = json.dumps(data, indent=4)
+df = pd.json_normalize(data[key])
 
-# Writing to sample.json
-with open("../sample.json", "w") as outfile:
-    outfile.write(json_object)
+df['timestamp'] = pd.to_datetime(datetime.now())
+
+print(df)
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:root123@localhost:3306/SQLDB')
+
+df.to_sql(table_name, engine, if_exists='replace', index=False)
